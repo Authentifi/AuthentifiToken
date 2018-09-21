857a59fc4d6cc29850363c629ac38c14fa56a7f5
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2018-09-21 17:03:51 -0400

    added some experimental ABI encoding for temporary purposes

--- contracts/AuthentifiCampaign.sol
+++ contracts/AuthentifiCampaign.sol
@@ -1,34 +1,51 @@
 pragma solidity ^0.4.21;
+pragma experimental ABIEncoderV2;
 
 contract AuthentifiCampaign {
 
+  event RecipientsSet(address _owner, bytes32 _name);
+  event AuditorsFilled(address _owner, bytes32 _name);
 
-  Struct Recipient { //Recipient information
+  struct Recipient { //Recipient information
     bytes32 name; //short name
     bytes32 contactInfo; //phone number
+    int[] auditorsToAssign;
   }
 
-  Struct Auditor { // Auditors
+  struct Auditor { // Auditors
     address auditorWallet;
     bool reserved;
     bytes32[] recipientList;
-    mapping(bytes32 => Recipient) RecipientStructs;
+    uint[] Recipients;
     mapping(bytes32 => RecipientAudit) AuditStructs;
   }
 
-  Struct RecipientAudit { //an Audit of a recipient
+  struct RecipientAudit { //an Audit of a recipient
     Recipient recipient; //the recipient
     uint checksum; //the checksummed value of what the auditor result was
     string result;
   }
 
   address public owner;
-  string public name;
+  bytes32 public name;
+  int public maxAuditors;
   mapping(address => Auditor) Auditors;
+  mapping(uint => Recipient) Recipients;
 
-  constructor()
+  constructor(bytes32 _name) public {
+    owner = msg.sender;
+    name = _name;
+  }
 
-
+  function setAuditorCount(int count) public {
+    if (msg.sender == owner) {
+      maxAuditors = count;
+    }
+  }
+  function setRecipients(Recipient[] _recipients) {
+    _recipients.forEach()
+    emit RecipientsSet(owner, name);
+  }
 
 
 }
