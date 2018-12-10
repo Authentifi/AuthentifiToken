682a6fea95d5459bd6706e33d5e785a4123d9091
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-12-10 12:11:36 -0500

    update migration

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,6 +1,9 @@
-var AuthentifiAuditToken = artifacts.require("AuthentifiAuditToken")
+var AuthentifiAuditToken = artifacts.require("AuthentifiAuditToken");
+var AuthentifiCampaign = artifacts.require("AuthentifiCampaign");
 
 
 module.exports = function(deployer) {
-  deployer.deploy(AuthentifiAuditToken, 500, "AuthentifiAuditToken", "AAT" );
+  return deployer.deploy(AuthentifiAuditToken, 500, "AuthentifiAuditToken", "AAT" ).then(function() {
+    return deployer.deploy(AuthentifiCampaign, "Campaign", AuthentifiAuditToken.deployed().address, 10, 'safdsfads' );
+  })
 }
--- migrations/3_deploy_test_campaign.js
+++
@@ -1,6 +0,0 @@
-var AuthentifiCampaign = artifacts.require("AuthentifiCampaign");
-var AuthentifiAuditToken = artifacts.require("AuthentifiAuditToken");
-
-module.exports = function(deployer) {
-  deployer.deploy(AuthentifiCampaign, "Campaign", AuthentifiAuditToken.address, 10 );
-}
