83bdd428ce98751356b8b684622b20d718cacfe4
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-12-10 10:08:42 -0500

    finalization and verification process

--- contracts/AuthentifiCampaign.sol
+++ contracts/AuthentifiCampaign.sol
@@ -8,6 +8,9 @@ contract AuthentifiCampaign {
   event AuditorComplete(address _auditor, bytes32 _hash);
   event AuditorsFilled(address _owner, bytes32 _name);
   event AuditorAdded(address _auditor, uint256 _tokens, address _thisOne);
+  event FullAuditComplete(uint _finalResult);
+  event AuditorPaid(address _auditor, uint256 _tokens);
+  event AuditorSlashed(address _auditor);
 
   struct Auditor { // Auditors
     address auditorWallet;
@@ -23,6 +26,9 @@ contract AuthentifiCampaign {
   uint public currentAuditorCount;
   uint256  public minimumTokens;
   mapping(address => Auditor) public Auditors;
+  address[] public AuditorList;
+  uint public recipientResults;
+  uint public finalResult;
   bytes32 public RecipientRootHash;
 
   constructor(bytes32 _name, address _acceptedTokens, uint256 _minTokens, bytes32 RootHash) public {
@@ -32,6 +38,7 @@ contract AuthentifiCampaign {
     auditTokenAddress = _acceptedTokens;
     minimumTokens = _minTokens;
     RecipientRootHash = RootHash;
+    finalResult = 0;
   }
 
   function setAuditorCount(uint count) public { // the required number of auditors for an audit
@@ -47,9 +54,26 @@ contract AuthentifiCampaign {
   }
 
   function uploadRecipientResults(bytes32 recipientResults) public {
-    if (Auditors[msg.sender]) {
+    if (Auditors[msg.sender] && !Auditors[msg.sender].RecipientResults) {
       Auditors[msg.sender].RecipientResults = recipientResults;
-      emit AuditorComplete(msg.sender, recipientResults)
+      recipientResults += 1;
+      emit AuditorComplete(msg.sender, recipientResults);
+    }
+    if (recipientResultsList.length >= maxAuditors) {
+      uint temp = 0;
+      for (uint i = 0; i < AuditorList.length; i++) {
+        temp += Auditors[AuditorList[i]].RecipientResults;
+      }
+      finalResult = temp / AuditorList.length;
+      for (uint j = 0; j < AuditorList.length; j++) {
+        if ( (finalResult - Auditors[AuditorList[j]].RecipientResults) < 10 && finalResult - Auditors[AuditorList[j]].RecipientResults) > -10 ) { //the results are within 10
+          auditContract.transfer(AuditorList[j], minimumTokens);
+          emit AuditorPaid(AuditorList[j], minimumTokens);
+        } else {
+          emit AuditorSlahed(AuditorList[j]);
+        }
+      }
+      emit FullAuditComplete(finalResult);
     }
   }
 
@@ -57,6 +81,7 @@ contract AuthentifiCampaign {
     if (auditContract.allowance(msg.sender, address(this))>= minimumTokens && currentAuditorCount < maxAuditors) {  //checks if the auditor count is low enough, and iterates the auditor count up by 1
       auditContract.transferFrom(msg.sender, address(this), minimumTokens);
       Auditors[msg.sender].auditorWallet = msg.sender;
+      AuditorList.push(msg.sender);
       emit AuditorAdded(msg.sender, minimumTokens, address(this));
     }
   }
